6bba5485aaf2fe0f0b5ef9dede78ad74edd11be7
xlthlx <xlthlx@gmail.com>
2021-06-26 17:34:53 +0100

    Update error pages

--- 404.php
+++ 404.php
@@ -8,21 +8,19 @@
 ?>
 <?php get_header(); ?>
 
-    <section id="not-found" class="pt-5">
+    <section id="not-found" class="pt-0">
         <div class="ms-auto text-end">
             <h2 class="mt-5 mb-3 h2-responsive">Uh-oh</h2>
         </div>
-        <hr class="dotted mb-5 mt-4">
+        <hr class="dotted mb-4 mt-4">
 
         <div class="row mt-3">
             <div class="col-md-12 mb-4">
                 <p class="text-center">The hamsters who run this site have not found what you were looking for.</p>
-                <p>
-                    <img class="img-fluid rounded z-depth-2 mx-auto d-block" src="<?php echo get_template_directory_uri(); ?>/assets/img/404.gif" alt="404"/>
+                    <p><img class="img-fluid rounded z-depth-2 mx-auto d-block" src="<?php echo get_template_directory_uri(); ?>/assets/img/404.gif" alt="404"/>
                 </p>
             </div>
         </div>
-        <hr/>
     </section>
 
 <?php get_footer();
