8d9d55ab6c42b02fcf41a362ea3c8270089edddd
xlthlx <xlthlx@gmail.com>
2022-02-15 09:19:03 +0000

    Test

--- header.php
+++ header.php
@@ -14,10 +14,9 @@
           content="Serena Piccioni, a Senior Developer with 20+ years of experience in the digital and information technology industry."/>
     <meta name="author" content="Serena Piccioni">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-		<?php echo esc_attr( get_bloginfo( 'description' ) ); ?> | <?php echo esc_attr( get_bloginfo( 'name' ) ); ?>
-    </title>
-    <link rel="author" href="<?php echo home_url( '/' ); ?>humans.txt"/>
+    <title><?php echo esc_attr( get_bloginfo( 'description' ) ); ?>
+        | <?php echo esc_attr( get_bloginfo( 'name' ) ); ?></title>
+    <link rel="author" href="<?php echo esc_url( home_url( '/' ) ); ?>humans.txt"/>
     <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/manifest/manifest.json"/>
     <script defer data-domain="piccioni.london" src="https://plausible.io/js/plausible.js"></script>
 	<?php wp_head(); ?>
