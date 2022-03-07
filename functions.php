8d7fc36091327714174cd482c8c07392df61906d
xlthlx <xlthlx@gmail.com>
2022-03-07 08:35:54 +0000

    Remove main css

--- functions.php
+++ functions.php
@@ -15,17 +15,12 @@ add_filter( 'login_display_language_dropdown', '__return_false' );
  */
 function london_core_scripts() {
 
-	wp_enqueue_style( 'main',
-		get_template_directory_uri() . '/assets/css/main.min.css', false, [],
-		'screen,print' );
 	wp_dequeue_style( 'wp-block-library' );
 
 	wp_deregister_script( 'jquery' );
 	wp_deregister_script( 'wp-embed' );
 	wp_deregister_script( 'wp-polyfill' );
-	wp_enqueue_script( 'main',
-		get_template_directory_uri() . '/assets/js/main.min.js', false, [],
-		true );
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.min.js', false, [], true );
 
 }
 
