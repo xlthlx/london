bdae634422882452b75ccdef509dbdc1ce3ca480
xlthlx <xlthlx@gmail.com>
2022-03-07 08:31:05 +0000

    Small fixes

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -87,7 +87,7 @@ add_filter( 'embed_oembed_discover', 'ln_restore_oembed_cache' );
  */
 function ln_insert_css() {
 	$file  = get_template_directory() . '/assets/css/main.min.css';
-	$style = str_replace( '../fonts/',
+	$style = str_replace( 'fonts/',
 		get_template_directory_uri() . '/assets/fonts/',
 		ln_get_file_content( $file ) );
 
@@ -95,32 +95,3 @@ function ln_insert_css() {
 }
 
 add_action( 'wp_head', 'ln_insert_css' );
-
-/**
- * Fix output duotone svg filters in footer.
- *
- * @link https://github.com/WordPress/gutenberg/issues/38299
- * @link https://core.trac.wordpress.org/ticket/54941
- *
- * @return void
- * @throws ReflectionException
- */
-function ln_fix_output_duotone_svg() {
-	$filters = $GLOBALS['wp_filter']['wp_footer'][10];
-	if ( empty( $filters ) ) {
-		return;
-	}
-
-	foreach ( $filters as $callback ) {
-		if ( $callback['function'] instanceof Closure ) {
-			$instance  = new \ReflectionFunction( $callback['function'] );
-			$variables = $instance->getStaticVariables();
-			if ( ! empty( $variables['svg'] ) && strpos( $variables['svg'],
-					'feColorMatrix' ) ) {
-				remove_action( 'wp_footer', $callback['function'] );
-			}
-		}
-	}
-}
-
-add_action( 'wp_footer', 'ln_fix_output_duotone_svg', 0 );
