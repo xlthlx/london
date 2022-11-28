fccc9827d462ab333ab988e21faac28008fffb1e
xlthlx <xlthlx@gmail.com>
2022-11-28 09:40:55 +0000

    Bug fix

--- inc/theme/index.php
+++ inc/theme/index.php
@@ -10,7 +10,7 @@
  */
 require_once ABSPATH . 'wp-admin/includes/file.php';
 
-$folder = get_template_directory() . '/inc/toolkit/inc/';
+$folder = get_template_directory() . '/inc/theme/inc/';
 $files  = list_files( $folder, 2 );
 foreach ( $files as $file ) {
 	if ( is_file( $file ) ) {
